65f849073ded6f8b57277e1b746ac0f40bf24f8a
GaskaPiotr <04piotr04@wp.pl>
2026-02-06 09:01:06 +0100

    Added BaseEntity with created_at and updated_at and User now extends BaseEntity

---
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.github.GaskaPiotr.spring_boot_boilerplate.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(name="created_at", updatable = false, nullable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private Instant updatedAt;
+}
--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/entity/User.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/entity/User.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
