a503e9d954f6341c20ec85d86063f4f9813048b2
GaskaPiotr <04piotr04@wp.pl>
2026-02-07 09:01:01 +0100

    Added UserRepository that extends JpaRepository

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/repository/UserRepository.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/repository/UserRepository.java
@@ -1,4 +1,9 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.repository;
 
-public class UserRepository {
+import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
