6f3d967d5d7ab978b54ca1687752bb71308c5016
GaskaPiotr <04piotr04@wp.pl>
2026-02-07 08:39:36 +0100

    Added record LoginRequest with email and password

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/LoginRequest.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.dto;
 
-public class LoginRequest {
-}
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+
+    @NotBlank(message="Email can't be empty!")
+    String email,
+
+    @NotBlank(message="Password can't be empty!")
+    String password
+) {}
