47db2c5f6572a485090dbc53a802d9ae60c6a74e
GaskaPiotr <04piotr04@wp.pl>
2026-02-09 08:01:39 +0100

    Added login method

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/controller/SecurityController.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/controller/SecurityController.java
@@ -1,4 +1,27 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.controller;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.LoginRequest;
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.LoginResponse;
+import com.github.GaskaPiotr.spring_boot_boilerplate.service.SecurityService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class SecurityController {
+    private final SecurityService securityService;
+
+    public SecurityController(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse response = securityService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
