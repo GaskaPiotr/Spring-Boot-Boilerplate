78047cf62bc898d464efba446b35f9cd1fc20576
GaskaPiotr <04piotr04@wp.pl>
2026-02-09 07:29:04 +0100

    Added half of login method in SecurityController, TODO generating JWT and LoginResponse

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/repository/UserRepository.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/service/SecurityService.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/service/SecurityService.java
@@ -1,4 +1,38 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.service;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.LoginRequest;
+import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
+import com.github.GaskaPiotr.spring_boot_boilerplate.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
 public class SecurityService {
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    UserRepository userRepository;
+
+    public SecurityService(
+            UserRepository userRepository,
+            BCryptPasswordEncoder bCryptPasswordEncoder
+        ) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public login(LoginRequest request) {
+        User user = userRepository
+                .findByEmail(request.email())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Email or Password"));
+
+
+        if (!bCryptPasswordEncoder.matches(request.password(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Email or Password");
+        }
+
+        // TODO
+        // Generate JWT
+        // return LoginResponse with token
+    }
 }
