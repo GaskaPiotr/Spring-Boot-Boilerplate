db1ec755d286241b75147b4d72a91e0122ec08a2
GaskaPiotr <04piotr04@wp.pl>
2026-02-09 09:11:53 +0100

    Added methods generateToken and extractEmail to JwtService

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/security/JwtService.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/security/JwtService.java
@@ -1,4 +1,41 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.security;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class JwtService {
+    @Value("${application.security.jwt.secret-key}")
+    private String SECRET_KEY;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("role", user.getRole().getName())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
 }
