fcab0cfb75901aceff419873b75e3827dec93c29
GaskaPiotr <04piotr04@wp.pl>
2026-02-12 20:40:26 +0100

    Added handling AuthenticationException to GlobalExceptionHandler

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
@@ -5,8 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.AuthenticationException;
 
 
 @RestControllerAdvice
@@ -14,10 +17,20 @@ public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    @ExceptionHandler({AuthenticationException.class})
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex) {
+
+        logger.warn("Warning with authenticating a user: {}", ex.getMessage());
+
+        ErrorResponse response = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "Invalid email or password");
+
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
 
-        logger.error("An unexpected error happened: {}", String.valueOf(ex));
+        logger.error("An unexpected error happened: ", ex);
 
         ErrorResponse response = new ErrorResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
