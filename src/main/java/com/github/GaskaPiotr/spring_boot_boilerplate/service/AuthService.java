f5d851bef3595f5e3e18c977f0ec5b93a3710322
GaskaPiotr <04piotr04@wp.pl>
2026-02-12 15:22:10 +0100

    Changed login throws UsernameNotFound if the repository can't find a user with email

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/service/AuthService.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/service/AuthService.java
@@ -3,11 +3,13 @@ package com.github.GaskaPiotr.spring_boot_boilerplate.service;
 import com.github.GaskaPiotr.spring_boot_boilerplate.dto.LoginRequest;
 import com.github.GaskaPiotr.spring_boot_boilerplate.dto.LoginResponse;
 import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
+import com.github.GaskaPiotr.spring_boot_boilerplate.exception.InvalidCredentialsException;
 import com.github.GaskaPiotr.spring_boot_boilerplate.repository.UserRepository;
 import com.github.GaskaPiotr.spring_boot_boilerplate.security.JwtService;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -38,9 +40,8 @@ public class AuthService {
                 new UsernamePasswordAuthenticationToken(request.email(), request.password())
         );
 
-        User user = userRepository
-                .findByEmail(request.email())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid Email or Password"));
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         String jwtToken = jwtService.generateToken(user);
         return new LoginResponse(jwtToken);
