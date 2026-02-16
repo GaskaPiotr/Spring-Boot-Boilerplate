2be7b87a598558fb70beb7f2bd6c0061b47eb0dd
GaskaPiotr <04piotr04@wp.pl>
2026-02-16 18:51:27 +0100

    Added constraints for email and password

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/RegisterRequest.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/RegisterRequest.java
@@ -1,6 +1,11 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.dto;
 
+import jakarta.validation.constraints.Email;
+
 public record RegisterRequest(
+        @Email
         String email,
+
+        @StrongPassword
         String password
 ) {}
