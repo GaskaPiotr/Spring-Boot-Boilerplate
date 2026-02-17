ccdb1f796f96d5f26bfcc6ae5ed2587daadb5ac5
GaskaPiotr <04piotr04@wp.pl>
2026-02-17 07:39:42 +0100

    Imported StrongPassword annotation to RegisterRequest

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/RegisterRequest.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.dto;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.annotation.StrongPassword;
 import jakarta.validation.constraints.Email;
 
 public record RegisterRequest(
