94dfd77c9ef5f2c31efa238f245b4834aa1541ec
GaskaPiotr <04piotr04@wp.pl>
2026-02-23 07:26:30 +0100

    Changed AuthenticationException import to the right one

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
@@ -5,11 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.naming.AuthenticationException;
 
 
 @RestControllerAdvice
