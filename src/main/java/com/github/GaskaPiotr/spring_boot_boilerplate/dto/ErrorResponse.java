1f16aaef678f82a965e836f80813c6c67ae7c0b2
GaskaPiotr <04piotr04@wp.pl>
2026-02-23 07:49:03 +0100

    Changed errors to map with list in ErrorResponse

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/ErrorResponse.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/ErrorResponse.java
@@ -3,6 +3,7 @@ package com.github.GaskaPiotr.spring_boot_boilerplate.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
@@ -10,13 +11,13 @@ public record ErrorResponse (
         int status,
         String message,
         Instant timestamp,
-        Map<String, String> errors
+        Map<String, List<String>> errors
 ) {
     public ErrorResponse(int status, String message) {
         this(status, message, Instant.now(), null);
     }
 
-    public ErrorResponse(int status, String message, Map<String, String> errors) {
+    public ErrorResponse(int status, String message, Map<String, List<String>> errors) {
         this(status, message, Instant.now(), errors);
     }
 }
--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/exception/GlobalExceptionHandler.java
@@ -6,15 +6,40 @@ import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(
+                new ErrorResponse(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "Validation failed",
+                        errors
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
 
     @ExceptionHandler({AuthenticationException.class})
     public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException ex) {
