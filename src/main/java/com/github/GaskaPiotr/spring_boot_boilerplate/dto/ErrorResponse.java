5ed52ef057324826c716860a4c2113b37246713a
GaskaPiotr <04piotr04@wp.pl>
2026-02-23 07:30:23 +0100

    Added Map<> errors and new constructor to ErrorResponse

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/ErrorResponse.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/dto/ErrorResponse.java
@@ -1,13 +1,22 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.dto;
 
-import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.time.Instant;
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public record ErrorResponse (
         int status,
         String message,
-        Instant timestamp
+        Instant timestamp,
+        Map<String, String> errors
 ) {
     public ErrorResponse(int status, String message) {
-        this(status, message, Instant.now());
+        this(status, message, Instant.now(), null);
+    }
+
+    public ErrorResponse(int status, String message, Map<String, String> errors) {
+        this(status, message, Instant.now(), errors);
     }
 }
