2d51c93c485d4fb711209434dc0c5e468a2eaff2
GaskaPiotr <04piotr04@wp.pl>
2026-02-26 09:45:00 +0100

    fix: add componentModel spring and mapping annotation

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/mapper/UserMapper.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/mapper/UserMapper.java
@@ -3,8 +3,10 @@ package com.github.GaskaPiotr.spring_boot_boilerplate.mapper;
 import com.github.GaskaPiotr.spring_boot_boilerplate.dto.UserResponse;
 import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target="role", source="role.name")
     UserResponse userToUserResponse(User user);
 }
