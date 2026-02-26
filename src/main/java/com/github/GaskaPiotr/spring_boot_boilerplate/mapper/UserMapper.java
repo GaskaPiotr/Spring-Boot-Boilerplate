439766302270dac7acfce8a39494f8f3c2a278a4
GaskaPiotr <04piotr04@wp.pl>
2026-02-26 08:35:47 +0100

    fet: add user to userResponse method

--- src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/mapper/UserMapper.java
+++ src/main/java/com/github/GaskaPiotr/spring_boot_boilerplate/mapper/UserMapper.java
@@ -1,8 +1,10 @@
 package com.github.GaskaPiotr.spring_boot_boilerplate.mapper;
 
+import com.github.GaskaPiotr.spring_boot_boilerplate.dto.UserResponse;
+import com.github.GaskaPiotr.spring_boot_boilerplate.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface UserMapper {
-
+    UserResponse userToUserResponse(User user);
 }
